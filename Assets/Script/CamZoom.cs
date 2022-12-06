817b1d4dd157c7880e86c0d364d223e982758f36
Milagroov <92451537+Milagroov@users.noreply.github.com>
2022-12-06 17:50:24 +0100

    Zoom a faire

---
+++ Assets/Script/CamZoom.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CamZoom : MonoBehaviour
+{
+
+    public Camera Camera1;
+    public GameObject button;
+    public int speed;
+
+    void update()
+    {
+   
+    }
+
+}
