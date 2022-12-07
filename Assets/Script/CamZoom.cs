c1720cec7c703c9ab0d7151ef7e9e88aa7ac3994
Milagroov <92451537+Milagroov@users.noreply.github.com>
2022-12-07 11:57:50 +0100

    update zoom + write

--- Assets/Script/CamZoom.cs
+++ Assets/Script/CamZoom.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 
 public class CamZoom : MonoBehaviour
 {
+    public bool ZoomActive = false;
+    public Vector3[] Target;
+    public Camera Cam;
+    public float Speed;
 
-    public Camera Camera1;
-    public GameObject button;
-    public int speed;
-
-    void update()
+    private void Start()
     {
-   
+        Cam = Camera.main;
+    }
+
+    public void LateUpdate()
+    {
+        if (ZoomActive)
+        {
+            Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, 3, Speed);
+            Cam.transform.position = Vector3.Lerp(Cam.transform.position, Target[1], Speed);
+        }
+        else
+        {
+            Cam.orthographicSize = Mathf.Lerp(Cam.orthographicSize, 5, Speed);
+            Cam.transform.position = Vector3.Lerp(Cam.transform.position, Target[0], Speed);
+        }
+    }
+
+    public void ChangeBool()
+    {
+        ZoomActive = true;
     }
 
 }
